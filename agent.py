dc6ecc4b063a5ad54f6634e081fdcad1f4767562
asdidkf <fernan.gautrin@hotmail.com>
2026-08-26 21:15:33 -0700

    feat: add initialization and evaluation functions in agent.py

--- agent.py
+++ agent.py
@@ -1,11 +1,21 @@
 import tetris
+import random
 
-class genetic_algorithm:
-    def __init__(self, agentes):
-        self.agentes = agentes
+def inicializar(n):
+    individuos = [] * n
+
+    for i in individuos:
+        i.append([random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1)], 0)
+
+    return individuos
+
+def evaluar(individuos):
+    for i in individuos:
+        i[1] = tetris.jugar(i[0], random.randint())
+        print(i)
 
 
-class agent:
-    def __init__(self, pesos, lineas):
-        self.pesos = pesos
-        self.lineas = lineas
+def main():
+    n = 100
+    individuos = inicializar(n)
+    evaluar(individuos)
