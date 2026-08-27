77ab0fcea59d9ad71fe530aeecc22504f253728c
asdidkf <fernan.gautrin@hotmail.com>
2026-08-26 21:40:03 -0700

    feat: add initialization and evaluation for individuals

--- agent.py
+++ agent.py
@@ -2,22 +2,24 @@ import tetris
 import random
 
 def inicializar(n):
-    individuos = [] * n
+    individuos = []
 
-    for i in individuos:
-        i.append([random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1)], 0)
-
+    for i in range(n):
+        individuos.append([[random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1)], 0])
+        
     return individuos
 
 def evaluar(individuos):
     for i in individuos:
-        i[1] = tetris.jugar(i[0], random.randint())
+        i[1] = tetris.jugar(i[0], 10000, random.randint(1,1000))
         print(i)
 
+    individuos.sort(key = lambda x:x[1])
 
 def main():
     n = 100
     individuos = inicializar(n)
     evaluar(individuos)
+    
 
 main()
