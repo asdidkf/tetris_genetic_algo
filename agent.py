4cc2dd1fe203a3997c8a479127429222c6cde379
asdidkf <fernan.gautrin@hotmail.com>
2026-08-26 20:49:52 -0700

    feat: create genetic algorithm and agent class and add needed attributes

---
+++ agent.py
@@ -0,0 +1,11 @@
+import tetris
+
+class genetic_algorithm:
+    def __init__(self, agentes):
+        self.agentes = agentes
+
+
+class agent:
+    def __init__(self, pesos, lineas):
+        self.pesos = pesos
+        self.lineas = lineas
